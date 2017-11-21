f6835ec5683128a10d51ff4e2479f64f8946797a
eddiechina <eddiechina@yahoo.com>
2017-11-21 17:54:46 +0800

    添加项目文件。

---
+++ NAAAS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NAAAS.Startup))]
+namespace NAAAS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
